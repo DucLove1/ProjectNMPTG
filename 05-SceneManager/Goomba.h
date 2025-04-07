487ca2fd1674e1da47c55c9f0786558cc4dc096c
LuNguyenDuyDuc <duclnd.t1.2023@gmail.com>
2025-04-07 15:18:18 +0700

    Make brown goomba and red goomba has wing and change to test in Mario class, add some characters in enemies.png file

--- 05-SceneManager/Goomba.h
+++ 05-SceneManager/Goomba.h
@@ -1,41 +1,76 @@
 #pragma once
 #include "GameObject.h"
-
-#define GOOMBA_GRAVITY 0.002f
+#include "CEnemy.h"
+#define GOOMBA_GRAVITY 0.0015f
 #define GOOMBA_WALKING_SPEED 0.05f
+#define GOOMBA_JUMPING_FORCE 0.08f
+#define GOOMBA_FLY_FORCE 0.3f
 
+#define GOOMBA_ALIVE_BBOX_WIDTH 16.0f
+#define GOOMBA_ALIVE_BBOX_HEIGHT 14.0f
+#define GOOMBA_DIE_BBOX_WIDTH 16.0f
+#define GOOMBA_DIE_BBOX_HEIGHT 9.0f
 
-#define GOOMBA_BBOX_WIDTH 16
-#define GOOMBA_BBOX_HEIGHT 14
-#define GOOMBA_BBOX_HEIGHT_DIE 7
+#define GOOMBA_SPRITE_WIDTH 16.0f
+#define GOOMBA_SPRITE_HEIGHT 14.0f
+#define GOOMBA_SPRITE_HEIGHT_WING 24.0f
+#define GOOMBA_SPRITE_WIDTH_WING 20.0f
+#define GOOMBA_SPRITE_HEIGHT_DIE 9.0f
 
 #define GOOMBA_DIE_TIMEOUT 500
 
-#define GOOMBA_STATE_WALKING 100
-#define GOOMBA_STATE_DIE 200
+#define TIME_JUMPING 1500
+#define TIME_FLYING 2000
+#define TIME_KNOCK_OUT 200
+#define TIME_OUT_KNOCK_OUT 5000
+#define GOOMBA 0
+#define RED_GOOMBA 1
+#include "AssetIDs.h"
+#include "GameObject.h"
+#include "Mario.h"
 
-#define ID_ANI_GOOMBA_WALKING 5000
-#define ID_ANI_GOOMBA_DIE 5001
-
-class CGoomba : public CGameObject
+class CGoomba : public CEnemy
 {
-protected:
-	float ax;				
-	float ay; 
-
+private:
+	ULONGLONG timerFly;
+	int type;
 	ULONGLONG die_start;
-
-	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
-	virtual void Render();
-
-	virtual int IsCollidable() { return 1; };
-	virtual int IsBlocking() { return 0; }
-	virtual void OnNoCollision(DWORD dt);
-
-	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
-
-public: 	
-	CGoomba(float x, float y);
-	virtual void SetState(int state);
+	int countJump;
+	ULONGLONG timerKnockOut;
+	void SetStateDie();
+	void SetStateHasNoWing();
+	void SetStateHasWing();
+	void SetStateKockOut();
+public:
+	enum State
+	{
+		HAS_WING,
+		HAS_NO_WING,
+		DIE,
+		KNOCK_OUT
+	};
+	CGoomba(float x, float y, int type = RED_GOOMBA, int state = HAS_WING) : CEnemy(x, y)
+	{
+		this->type = type;
+		this->state = -1;
+		SetState(state);
+		this->ay = GOOMBA_GRAVITY;
+		this->ax = 0;
+		this->vx = -GOOMBA_WALKING_SPEED;
+		this->onGround = false;
+		timerFly = 0;
+		countJump = 0;
+	}
+	void SetState(int state) override;
+	void GetBoundingBox(float& left, float& top, float& right, float& bottom) override;
+	void OnNoCollision(DWORD dt) override;
+	void OnCollisionWith(LPCOLLISIONEVENT e) override;
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) override;
+	void UpdateState();
+	void Render() override;
+	void KickedFromTop(CGameObject*);
+	void KnockedOut(CGameObject*);
+	void TouchTwoSide(CGameObject*) {};
+	int IsCollidable() override { return state != KNOCK_OUT; }
+	bool IsAlive() override { return state != DIE && state != KNOCK_OUT; };
 };
